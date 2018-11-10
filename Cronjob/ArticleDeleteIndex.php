3b0480519aeab48edce4264a287181edbe8874c5
fleurami <38048487+fleurami@users.noreply.github.com>
2018-11-10 11:42:11 +0100

    Update ArticleDeleteIndex.php

--- Cronjob/ArticleDeleteIndex.php
+++ Cronjob/ArticleDeleteIndex.php
@@ -2,7 +2,7 @@
 // load oxid framework
 require_once(dirname(__FILE__). "/../../../../bootstrap.php");
 $result = \OxidCommunity\Elasticsearch\Application\Controller\Admin\ElasticsearchCron::DeleteArticleIndex();
-if ($result['acknowleged'] == '1') {
+if ($result == '1') {
     echo 'Done';
 } else {
     echo $result;
